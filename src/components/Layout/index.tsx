ea60f57613f908135a55dcbce25dbfd03004ae67
Victor Whyte <victordavidwhyte@gmail.com>
2025-05-09 19:49:43 +0100

    feat: remove preloader

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,20 +2,20 @@
 
 import StyledComponentsRegistry from '../../../libs/registry';
 import { GlobalStyles } from './GlobalStyles';
-import { Footer, Header, Preloader } from '..';
-import { useState } from 'react';
+import { Footer, Header } from '..';
+// import { useState } from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const [complete, setComplete] = useState(false);
+  // const [complete, setComplete] = useState(false);
   return (
     <StyledComponentsRegistry>
         <GlobalStyles />
-        <Preloader setComplete={setComplete} />
-        <div className={complete ? 'complete' : 'not_complete'}>
+        {/* <Preloader setComplete={setComplete} /> */}
+        {/* <div className={complete ? 'complete' : 'not_complete'}> */}
           <Header />
           {children}
           <Footer />
-        </div>
+        {/* </div> */}
     </StyledComponentsRegistry>
   );
 };
